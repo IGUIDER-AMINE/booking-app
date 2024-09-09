64356c8074af66fb3068de9a70080bdc34754468
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-09-09 11:58:44 +0200

    v2

--- api/index.js
+++ api/index.js
@@ -124,7 +124,6 @@ app.post("/upload", photosMiddleware.array("photos", 100), async (req, res) => {
 });
 
 app.post("/places", (req, res) => {
-  mongoose.connect(process.env.MONGO_URL);
   const { token } = req.cookies;
   const {
     title,
@@ -157,5 +156,13 @@ app.post("/places", (req, res) => {
   });
 });
 
+app.get("/places", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
 app.listen(4000);
-//03:40:03
+//04:11:04
